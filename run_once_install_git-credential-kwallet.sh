920fc076c2ed66bbefcd0cab90566f7affc6d516
Miren Radia <miren_radia@yahoo.co.uk>
2026-05-19 13:46:25 +0100

    Switch to libsecret git credential helper

--- run_once_install_git-credential-kwallet.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# From https://codeberg.org/mehrad/git-credential-kwallet
-wget -O "${HOME}/.local/bin/git-credential-kwallet" \
-'https://codeberg.org/mehrad/git-credential-kwallet/raw/branch/main/git-credential-kwallet' \
-&& chmod 0755 "${HOME}/.local/bin/git-credential-kwallet"
